d78a66845ad0512d41c661bea4197d33fea1f41d
vidya91-vv <venvidyashree@gmail.com>
2023-04-07 14:12:03 +0530

    Class

---
+++ Class/Dog.java
@@ -0,0 +1,12 @@
+package Class;
+
+public class Dog {
+  public String breed = "German shepard";
+  public int hight = 35;
+  public int wight = 30;
+
+  public static void main(String[] args){
+    Dog dog = new Dog();
+    System.out.println(dog.breed);
+  }
+}
