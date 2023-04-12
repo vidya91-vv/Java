75574036aa631fcce1f9ba80fc934fc5b4aca12b
vidya91-vv <venvidyashree@gmail.com>
2023-04-12 14:07:20 +0530

    Polymorpism

---
+++ Polymorphism/Calculations.java
@@ -0,0 +1,21 @@
+package Polymorphism;
+
+public class Calculations {
+
+  public void add(int a, int b){
+    //add method code
+  }
+
+  public void  add(int x, int a, int c){
+
+  }
+
+  public static void main(String[]args){
+    Calculations c = new Calculations();
+
+    c.add(2,3);
+    c.add(2,3,4);
+
+
+  }
+}
